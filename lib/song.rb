8bf82143197d7abb3c771cf156e3bbd208d20721
Sean Good <tree662@protonmail.com>
2019-11-21 18:13:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,14 @@ class Song
   
   def self.genre_count
     genre_count = {}     
-    
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else 
+        genre_count[genres] = 1 
+      end
+    end
+    genre_count
     # dup = nil
     # @@genres.each do |genre|           
     #   genre_count[genre] = (genre_count[genre] || 0 ) + 1
